cf9b8c0a4acfea85c6ae159d898ad97116e8be19
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-06-09 15:15:53 +0530

    improves loop media page logic (ui & state)

--- lib/routes/loop_video.dart
+++ lib/routes/loop_video.dart
@@ -1,273 +1,176 @@
 import 'dart:io';
 
+import 'package:animate_do/animate_do.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:osumffmpeg/components/durationfield.dart';
 
-TimeOfDay getTimeOfDayFromString(final String timeString) {
-  final timeString = TimeOfDay.now().toString();
-  final parts = timeString.split(':');
-  final hours = int.parse(parts.first.split('(').last);
-  final minutes = int.parse(parts.skip(1).first.split(')').first);
-  return TimeOfDay(hour: hours, minute: minutes);
-}
+import 'package:path/path.dart';
 
-// String getStringFromTimeOfDay(final TimeOfDay time) {
-//   return time.hour +
-// }
+import '../components/custom_button.dart';
+import '../components/duration_field.dart';
+import '../components/head_text.dart';
+import '../engine/media.dart';
 
-class LoopVideoPage extends HookWidget {
-  const LoopVideoPage({super.key});
+class _PageState {
+  /// The loop media page state.
+  _PageState()
+      : input = useTextEditingController(),
+        output = useTextEditingController(),
+        duration = DurationController(),
+        enableLoop = useState(false);
 
-  Future<void> onBrowseInputFile(
-    final TextEditingController input,
-  ) async {
-    final file = await FilePicker.platform.saveFile();
+  /// The [input] media file text field controller.
+  final TextEditingController input;
+
+  /// The [output] looped media file text field controller.
+  final TextEditingController output;
+
+  /// The [duration] of the looped media file.
+  final DurationController duration;
+
+  /// Should the loop button be enabled.
+  final ValueNotifier<bool> enableLoop;
+
+  /// Get the default output file location based on the [input] given.
+  ///
+  /// Example
+  ///   `C:\Users\User\Desktop\sample.mp3` becomes -->
+  ///   `C:\Users\User\Desktop\sample_looped.mp3`
+  String getDefaultOutputFileLocation() {
+    final path = dirname(input.text);
+    final filename = basenameWithoutExtension(input.text);
+    final ext = extension(input.text);
+
+    return '$path/${filename}_looped$ext';
+  }
+
+  /// On browsing the input file to loop.
+  Future<void> onBrowseInputFile() async {
+    final file = await FilePicker.platform.saveFile(
+      dialogTitle: 'Pick input media file.',
+    );
 
     if (file != null) {
       input.text = file;
+
+      final totalDuration = await Media(File(file)).getDuration();
+
+      duration.hours.text = '${totalDuration.inHours.remainder(24)}';
+      duration.minutes.text = '${totalDuration.inMinutes.remainder(60)}';
+
+      output.text = getDefaultOutputFileLocation();
     }
   }
 
-  void onBrowseOutputLocation() {}
+  /// On saving looped output media file.
+  Future<void> onBrowseOutputFile() async {
+    late String? file;
 
-  Future<void> onChangeLoopDuration(
-    final BuildContext context,
-    final TextEditingController durationHour,
-    final TextEditingController durationMinutes,
-  ) async {
-    // final value = await showTimePicker(
-    //   context: context,
-    //   initialTime: TimeOfDay.now(),
-    //   initialEntryMode: TimePickerEntryMode.input,
-    //   onEntryModeChanged: null,
-    //   builder: (final context, final child) => MediaQuery(
-    //     data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-    //     child: child!,
-    //   ),
-    // );
-    // if (value != null) {
-    //   durationHour.text = '${value.hour}';
-    //   durationMinutes.text = '${value.minute}';
-    // }
+    if (await File(output.text).exists()) {
+      file = await FilePicker.platform.saveFile(
+        dialogTitle: 'Pick output media file.',
+        initialDirectory: dirname(output.text),
+      );
+    } else {
+      file = await FilePicker.platform.saveFile(
+        dialogTitle: 'Pick output media file.',
+      );
+    }
+
+    if (file != null) {
+      output.text = file;
+    }
   }
+
+  /// Checking if the form is valid to enable loop button.
+  Future<void> onChangeInForm() async {
+    if (await File(input.text).exists() &&
+        await Directory(dirname(input.text)).exists()) {
+      final hours = int.tryParse(duration.hours.text);
+      final minutes = int.tryParse(duration.minutes.text);
+
+      if (hours != null && minutes != null) {
+        enableLoop.value = true;
+      }
+    } else {
+      enableLoop.value = false;
+    }
+  }
+}
+
+class LoopMediaPage extends HookWidget {
+  /// The loop media page can be used by the user to loop out their
+  /// media files by specifying the output video duration.
+  const LoopMediaPage({super.key});
 
   @override
   Widget build(final BuildContext context) {
-    final input = useTextEditingController();
-    final output = useTextEditingController();
-    final durationHours = useTextEditingController();
-    final durationMinutes = useTextEditingController();
+    final state = _PageState();
 
-    final ffmpegOutput = useState<StreamBuilder<List<int>>?>(null);
-
-    return Scrollbar(
-      child: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Input media file.',
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              Row(
+    return FadeInRight(
+      duration: const Duration(milliseconds: 200),
+      child: Form(
+        onChanged: state.onChangeInForm,
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(
-                    child: TextField(
-                      controller: input,
-                      enabled: false,
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton.icon(
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                        const EdgeInsets.all(16),
+                  const HeadingText('Input media file.'),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          controller: state.input,
+                          enabled: false,
+                        ),
                       ),
-                    ),
-                    onPressed: () => onBrowseInputFile(input),
-                    icon: const Icon(Icons.file_copy_outlined),
-                    label: const Text('Browse'),
-                  )
+                      const SizedBox(width: 10),
+                      CustomButton(
+                        onPressed: state.onBrowseInputFile,
+                        icon: const Icon(Icons.file_copy_outlined),
+                        label: 'Browse',
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const HeadingText('Loop video for'),
+                  const SizedBox(height: 20),
+                  DurationField(controller: state.duration),
+                  const SizedBox(height: 20),
+                  const HeadingText('Output media file.'),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          controller: state.output,
+                          enabled: false,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      CustomButton(
+                        icon: const Icon(Icons.save),
+                        label: 'Save Location',
+                        onPressed: state.onBrowseOutputFile,
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  CustomButton(
+                    label: 'Loop',
+                    icon: const Icon(Icons.loop),
+                    onPressed: state.enableLoop.value ? () {} : null,
+                  ),
+                  const SizedBox(height: 10),
                 ],
               ),
-              const SizedBox(height: 20),
-              Text(
-                'Loop video for',
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              const SizedBox(height: 20),
-              DurationField(
-                hours: durationHours,
-                minutes: durationMinutes,
-                onTap: () => onChangeLoopDuration(
-                  context,
-                  durationHours,
-                  durationMinutes,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'Output media file.',
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: output,
-                      enabled: false,
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton.icon(
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                        const EdgeInsets.all(16),
-                      ),
-                    ),
-                    onPressed: () {},
-                    icon: const Icon(Icons.save),
-                    label: const Text('Save Location'),
-                  )
-                ],
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton.icon(
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all(
-                    const EdgeInsets.all(16),
-                  ),
-                ),
-                onPressed: () {},
-                icon: const Icon(Icons.send),
-                label: const Text('Convert'),
-              ),
-              const SizedBox(height: 10),
-              // if (validatedStatus != 'Success') Text(formNotifier.validate()),
-              // if (ffmpegOutput != null) ffmpegOutput,
-            ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-// class LoopVideoPage extends ConsumerStatefulWidget {
-//   const LoopVideoPage({super.key});
-
-//   @override
-//   ConsumerState<ConsumerStatefulWidget> createState() => _LoopVideoPageState();
-// }
-
-// class _LoopVideoPageState extends ConsumerState<LoopVideoPage> {
-//   late TextEditingController inputFileController;
-//   late TextEditingController loopDurationController;
-//   late TextEditingController outputFileController;
-
-//   @override
-//   void initState() {
-//     inputFileController = TextEditingController();
-//     outputFileController = TextEditingController();
-//     loopDurationController = TextEditingController();
-//     super.initState();
-//   }
-
-//   @override
-//   void dispose() {
-//     inputFileController.dispose();
-//     outputFileController.dispose();
-//     loopDurationController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(final BuildContext context) {
-//     return
-//   }
-// }
-
-// class LoopVideoPage extends ConsumerWidget {
-//   const LoopVideoPage({super.key});
-
-//   void onConvert(
-//     final FFmpegProvider ffmpegNotifier,
-//     final ConvertMediaState formState,
-//   ) {
-//     ffmpegNotifier.sendToFFmpeg(
-//       ['-i', formState.inputToString(), formState.outputToString(), '-y'],
-//     );
-//   }
-
-//   Future<void> onBrowse(
-//     final ConvertMediaFormState formControllers,
-//     final ConvertMediaProvider formNotifier,
-//   ) async {
-//     final result = await FilePicker.platform.pickFiles();
-
-//     if (result != null) {
-//       final file = File(result.files.single.path!);
-//       formControllers.input.text = file.absolute.path;
-//       formNotifier
-//         ..updateInputFile(file)
-//         ..autoUpdateOutputDirectory(
-//           formControllers.output,
-//         );
-//     }
-//   }
-
-//   void onFormatChanged(
-//     final String? value,
-//     final ConvertMediaProvider formNotifier,
-//   ) {
-//     if (value != null) {
-//       formNotifier.updateExtension(
-//         MediaFormats.values.firstWhere(
-//           (final format) => format.toString() == value,
-//           orElse: () {
-//             if (kDebugMode) {
-//               print(
-//                 'Unable to find media format from enum. '
-//                 'Falling back to m4v.',
-//               );
-//             }
-//             return MediaFormats.mk4;
-//           },
-//         ),
-//       );
-//     }
-//   }
-
-//   Future<void> onSaveLocation(
-//     final ConvertMediaFormState formControllers,
-//     final ConvertMediaProvider formNotifier,
-//   ) async {
-//     final location = await FilePicker.platform.getDirectoryPath();
-//     if (location != null) {
-//       formNotifier.updateOutputDirectory(Directory(location));
-//       formControllers.output.text = location;
-//     }
-//   }
-
-//   @override
-//   Widget build(final BuildContext context, final WidgetRef ref) {
-//     // FORM CONTROLLERS
-//     final formControllers = ref.watch(ConvertMediaControllersProvider.provider);
-
-//     // LOGIC PARAMETERS
-//     final formState = ref.watch(ConvertMediaProvider.provider);
-//     final formNotifier = ref.watch(ConvertMediaProvider.provider.notifier);
-
-//     // FFMPEG
-//     final ffmpegStatus = ref.watch(FFmpegProvider.provider);
-//     final ffmpegNotifier = ref.watch(FFmpegProvider.provider.notifier);
-
-//     final validatedStatus = formNotifier.validate();
-
-//     return Container();
-//   }
-// }
