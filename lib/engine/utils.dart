94909d04b14254db09cf5eb529425361722697dd
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-06-10 21:34:53 +0530

    adds `protectUserPath` to censor user path in logs

--- lib/engine/utils.dart
+++ lib/engine/utils.dart
@@ -7,3 +7,7 @@ TimeOfDay getTimeOfDayFromString(final String timeString) {
   final minutes = int.parse(parts.skip(1).first.split(')').first);
   return TimeOfDay(hour: hours, minute: minutes);
 }
+
+List<String> protectUserPath(final List<String> commands) => commands
+    .map((final e) => e.contains('/') || e.contains(r'\') ? '***' : e)
+    .toList();
