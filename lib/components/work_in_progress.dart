6320c156f984631ef6a23a122aca7523c7b6139f
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-06-15 18:57:17 +0530

    adds `WorkInProgress`

---
+++ lib/components/work_in_progress.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:osumffmpeg/components/head_text.dart';
+
+class WorkInProgress extends StatelessWidget {
+  const WorkInProgress({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: const [
+          SizedBox(height: 30),
+          Icon(Icons.construction, size: 90,),
+          HeadingText("Work in progress"),
+        ],
+      ),
+    );
+  }
+}
