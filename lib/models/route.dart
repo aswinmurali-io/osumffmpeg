963d2c05354bfa56d42035d1a4637b5d5314b04e
Aswin Murali <aswin.murali2019@vitstudent.ac.in>
2022-06-24 08:12:20 +0530

    formats `Route`

--- lib/models/route.dart
+++ lib/models/route.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a GNU Lesser General Public License
 // v2.1 that can be found in the LICENSE file.
 
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:web_smooth_scroll/web_smooth_scroll.dart';
@@ -41,77 +40,72 @@ class Route implements WithDisplayableValue, WithDisplayString {
         child: SingleChildScrollView(
           physics: const NeverScrollableScrollPhysics(),
           controller: controller,
-          child: FadeInRight(
-            duration: const Duration(milliseconds: 200),
-            child: Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(3, 0, 0, 18),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        SizedBox(
-                          width: ThemeProvider.isMobile
-                              ? MediaQuery.of(context).size.width / 1.3
-                              : 400,
-                          child: TypeAheadFormField<Route>(
-                            textFieldConfiguration: TextFieldConfiguration(
-                              controller: OsumLayout.routeJumpController,
-                              decoration: InputDecoration(
-                                prefixIcon: const Icon(Icons.search),
-                                hintText:
-                                    'Click here to search for any feature.',
-                                hintStyle: Theme.of(context)
-                                    .inputDecorationTheme
-                                    .hintStyle
-                                    ?.copyWith(fontWeight: FontWeight.bold),
-                                enabledBorder: Theme.of(context)
-                                    .inputDecorationTheme
-                                    .enabledBorder
-                                    ?.copyWith(
-                                      borderSide: const BorderSide(
-                                        style: BorderStyle.none,
-                                      ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(3, 0, 0, 18),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: ThemeProvider.isMobile
+                            ? MediaQuery.of(context).size.width / 1.3
+                            : 400,
+                        child: TypeAheadFormField<Route>(
+                          textFieldConfiguration: TextFieldConfiguration(
+                            controller: OsumLayout.routeJumpController,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.search),
+                              hintText: 'Click here to search for any feature.',
+                              hintStyle: Theme.of(context)
+                                  .inputDecorationTheme
+                                  .hintStyle
+                                  ?.copyWith(fontWeight: FontWeight.bold),
+                              enabledBorder: Theme.of(context)
+                                  .inputDecorationTheme
+                                  .enabledBorder
+                                  ?.copyWith(
+                                    borderSide: const BorderSide(
+                                      style: BorderStyle.none,
                                     ),
-                                // focusedBorder: const UnderlineInputBorder(),
-                              ),
+                                  ),
+                              // focusedBorder: const UnderlineInputBorder(),
                             ),
-                            suggestionsCallback: (pattern) => OsumLayout.routes
-                                .where((route) => '$route'.contains(pattern))
-                                .map((route) => route.value),
-                            itemBuilder: (_, suggestion) => ListTile(
-                              leading: suggestion.icon,
-                              title: Text(suggestion.title),
-                              subtitle: Text(suggestion.description),
-                              isThreeLine: true,
-                            ),
-                            onSuggestionSelected: (suggestion) {
-                              index.value = OsumLayout.routes.indexWhere(
-                                (route) => route.title == suggestion.title,
-                              );
-                            },
-                            validator: (value) =>
-                                (value != null && value.isEmpty)
-                                    ? 'Please select a option.'
-                                    : null,
-                            onSaved: (value) {
-                              if (value != null) {
-                                OsumLayout.routeJumpController.text = value;
-                              }
-                            },
                           ),
+                          suggestionsCallback: (pattern) => OsumLayout.routes
+                              .where((route) => '$route'.contains(pattern))
+                              .map((route) => route.value),
+                          itemBuilder: (_, suggestion) => ListTile(
+                            leading: suggestion.icon,
+                            title: Text(suggestion.title),
+                            subtitle: Text(suggestion.description),
+                            isThreeLine: true,
+                          ),
+                          onSuggestionSelected: (suggestion) {
+                            index.value = OsumLayout.routes.indexWhere(
+                              (route) => route.title == suggestion.title,
+                            );
+                          },
+                          validator: (value) => (value != null && value.isEmpty)
+                              ? 'Please select a option.'
+                              : null,
+                          onSaved: (value) {
+                            if (value != null) {
+                              OsumLayout.routeJumpController.text = value;
+                            }
+                          },
                         ),
-                        const SizedBox(width: 10),
-                        const ThemeButton(),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(width: 10),
+                      const ThemeButton(),
+                    ],
                   ),
-                  content
-                ],
-              ),
+                ),
+                content
+              ],
             ),
           ),
         ),
